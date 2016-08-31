889fb8bac15619f82ce039641269da75a4f7f9ce
Mark Brillo <brillomark@yahoo.com>
2016-08-31 16:20:24 +0800

    c

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -8,21 +8,21 @@
 <script src="https://code.jquery.com/jquery-3.1.0.js"   integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
 
 
-{{ HTML::script('js/jquery.min.js') }}
+{{ HTML::script('//js/jquery.min.js') }}
 
 
 
-{{ HTML::script('js/bootstrap.min.js') }}
+{{ HTML::script('//js/bootstrap.min.js') }}
 
-{{ HTML::script('js/dist/sweetalert.min.js') }}
-
-
+{{ HTML::script('//js/dist/sweetalert.min.js') }}
 
 
 
 
 
-<script src="https://code.jquery.com/jquery-3.1.0.js"   integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
+
+
+<script src="//code.jquery.com/jquery-3.1.0.js"   integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
 
 
 <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
