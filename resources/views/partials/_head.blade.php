f6a836fbedf6833b1cd49d03418b8b828d072df6
Mark Brillo <brillomark@yahoo.com>
2016-09-06 16:16:30 +0800

    fixed bug

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,13 +4,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>@yield('title')</title>
 
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 {{ HTML::style('css/normalize.css') }}
 
-{{ HTML::style('css/bootstrap.min.css') }}
+{{--{{ HTML::style('css/bootstrap.min.css') }} --}}
     
 {{ HTML::style('css/styles.css') }}
 
 {{ HTML::style('css/dist/sweetalert.css') }}
+
+
 
 
 <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -8,21 +8,24 @@
 <script src="//code.jquery.com/jquery-3.1.0.js"   integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
 
 
-{{ HTML::script('/js/jquery.min.js') }}
+{{--{{ HTML::script('/js/jquery.min.js') }} --}}
 
 
 
-{{ HTML::script('js/bootstrap.min.js') }}
+{{--{{ HTML::script('js/bootstrap.min.js') }} --}}
 
-{{ HTML::script('js/dist/sweetalert.min.js') }}
-
-
+{{--{{ HTML::script('js/dist/sweetalert.min.js') }} --}}
 
 
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
 
 
-<script src="//code.jquery.com/jquery-3.1.0.js"   integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
+
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
 <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
